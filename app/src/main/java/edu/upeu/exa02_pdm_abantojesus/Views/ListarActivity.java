31b8a4abb4ae48619838dd15d46de3296de34268
JEMA <abantybredda@gmail.com>
2018-04-30 23:58:48 -0500

    2st Change

--- app/src/main/java/edu/upeu/exa02_pdm_abantojesus/Views/ListarActivity.java
+++ app/src/main/java/edu/upeu/exa02_pdm_abantojesus/Views/ListarActivity.java
@@ -1,16 +1,20 @@
 package edu.upeu.exa02_pdm_abantojesus.Views;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import edu.upeu.exa02_pdm_abantojesus.R;
 
-public class MainActivity extends AppCompatActivity {
+
+public class ListarActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listar);
 
+        rv = (RecyclerView) findViewById(R.id.recycler);
     }
 }
---
+++ app/src/main/java/edu/upeu/exa02_pdm_abantojesus/Views/LoginActivity.java
@@ -0,0 +1,97 @@
+package edu.upeu.exa02_pdm_abantojesus.Views;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import edu.upeu.exa02_pdm_abantojesus.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnlogin,btnfacebook,btngoogle;
+    private EditText eduser, edpass;
+    private TextView txtforgetpass,txtcreateacount;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnlogin = (Button) findViewById(R.id.login);
+        btnfacebook = (Button) findViewById(R.id.loginFace);
+        btngoogle = (Button) findViewById(R.id.loginGoogle);
+        eduser = (EditText) findViewById(R.id.username);
+        edpass = (EditText) findViewById(R.id.password);
+        txtforgetpass = (TextView) findViewById(R.id.forgotAccount);
+        txtcreateacount = (TextView) findViewById(R.id.createHere);
+
+        btnlogin.setOnClickListener(this);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null){
+                    Log.i("SESION","sesion iniciada con email: "+user.getEmail());
+                }else{
+                    Log.i("SESION", "sesion cerrada");
+                }
+            }
+        };
+    }
+
+    private void IniciarSesion(String email, String pass){
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Sesion Iniciada", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, ListarActivity.class));
+                }else{
+                    Toast.makeText(LoginActivity.this, "Sesion erronea no pudo iniciar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                String email = eduser.getText().toString();
+                String pass = edpass.getText().toString();
+                IniciarSesion(email,pass);
+            break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener!=null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
+}
